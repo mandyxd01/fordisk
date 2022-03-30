e2f149dc89a6b5a882d78fd7e7eca80e9076f17d
mandyxd01 <98300527+mandyxd01@users.noreply.github.com>
2022-03-30 01:52:39 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,11 +11,40 @@ hash = "d7c97d558577a8633485c557a41174ef"
 print("Starting Deployment..!")
 
 client = TelegramClient("main_session"  , api_id=id , api_hash=hash)
-indchats = [-1001599913233,-1001234716701,-1001493682832,-1001562499878,-1001168213397,-1001160036259]
-mdisk_api = 'jNgyWyCG2KK4tvh5RVFX'
-indsend_to = -1001494780519
 
-footer = "\n━━━━━━━━━━━━━━━\n⚙️ How to Download / Watch Online\n━━━━━━━━━━━━━━━\n⭐️JOIN CHANNEL ➡️ t.me/X3Links"
+#mdiskapi
+mdisk_api = 'jNgyWyCG2KK4tvh5RVFX'
+
+#footer
+footer = '''\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+⭐️JOIN OUR BACKUP CHANNEL
+➡️https://t.me/+S4m4SxWBsss0NGYx'''
+
+#indian variables
+indchats = [-1001794604239,-1001665192861,-1001626636022,-1001168213397,-1001708628164,-1001562499878,-1001493682832]
+indsend_to = -1001607066545
+
+#webseries var 
+webchat = [-1001634910114,-1001038963647,-1001350448575,-1001585256875,-1001398939317]
+websend_to = -1001671942243
+
+#desi vars
+deschat = [-1001387860550,-1001250363281,-1001314689320,-1001138671226,-1001461291601]
+dessend_to = -1001494780519
+
+#tango vars
+tanchat = [-1001785029855,-1001198221154,-1001745646849,-1001577675291,-1001568191558,-1001589153647]
+tansend_to = -1001735663681
+
+
+#onlyfans vars  
+onchat = [-1001608452561,-1001199943325,-1001491824023]
+onsend_to = -1001717209783   
+
+
+
+
+############### ENGLISH ##########
 
 @client.on(events.NewMessage(chats=indchats))
 async def hello(event):
@@ -45,7 +74,7 @@ async def hello(event):
         # url to change 
         for i in urls_to_change:
             link = regrex_pattern.sub(r'' , i)
-            print(link)
+            #print(link)
             url  = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
             param = {
                 'token': mdisk_api,
@@ -64,13 +93,213 @@ async def hello(event):
         else:
             await client.send_message(indsend_to , caption)
 
-    
+  ##################### WEBSERIES #############  
+
+@client.on(events.NewMessage(chats=webchat))
+async def hello(event):
+    # chat = await event.get_chat()
+    caption = event.message.message
+    urls_to_change = re.findall('https?://mdisk.me/convertor/.*' , caption)
+    if(urls_to_change):
+        try:
+            media = await client.download_media(event.message)
+        except:
+            print("no media")
+        #  this is for blacklist word 
+        caption = re.sub("hehe" , "" , caption)
+        caption = re.sub("hoho" , "" , caption)
+
+        caption = re.sub("@.*" , "" , caption)
+        caption = re.sub("https://t.me/.*" , "" , caption)
+        caption = re.sub("t.me/.*" , "" , caption)
+
+        regrex_pattern = re.compile(pattern = "["
+                    u"\U0001F600-\U0001F64F"  # emoticons
+                    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                    u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                            "]+", flags = re.UNICODE)
+
+        # url to change 
+        for i in urls_to_change:
+            link = regrex_pattern.sub(r'' , i)
+            print(link)
+            url  = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
+            param = {
+                'token': mdisk_api,
+                'link':link
+                }
+            res = requests.post(url, json = param)
+            shareLink = res.json()['sharelink']
+            # print("changed link : " , shareLink)
+            caption = re.sub(link , shareLink , caption)
+            # print(caption)
+            sleep(0.2)
+        caption = caption + "\n" + footer
+        if media:
+            await client.send_file(websend_to ,file=media , caption=caption)
+            os.remove(media)
+        else:
+            await client.send_message(websend_to , caption)
+            
+            
+            
+############# DESI #################
+@client.on(events.NewMessage(chats=deschat))
+async def hello(event):
+    # chat = await event.get_chat()
+    caption = event.message.message
+    urls_to_change = re.findall('https?://mdisk.me/convertor/.*' , caption)
+    if(urls_to_change):
+        try:
+            media = await client.download_media(event.message)
+        except:
+            print("no media")
+        #  this is for blacklist word 
+        caption = re.sub("hehe" , "" , caption)
+        caption = re.sub("hoho" , "" , caption)
+
+        caption = re.sub("@.*" , "" , caption)
+        caption = re.sub("https://t.me/.*" , "" , caption)
+        caption = re.sub("t.me/.*" , "" , caption)
+
+        regrex_pattern = re.compile(pattern = "["
+                    u"\U0001F600-\U0001F64F"  # emoticons
+                    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                    u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                            "]+", flags = re.UNICODE)
+
+        # url to change 
+        for i in urls_to_change:
+            link = regrex_pattern.sub(r'' , i)
+            print(link)
+            url  = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
+            param = {
+                'token': mdisk_api,
+                'link':link
+                }
+            res = requests.post(url, json = param)
+            shareLink = res.json()['sharelink']
+            # print("changed link : " , shareLink)
+            caption = re.sub(link , shareLink , caption)
+            # print(caption)
+            sleep(0.2)
+        caption = caption + "\n" + footer
+        if media:
+            await client.send_file(dessend_to ,file=media , caption=caption)
+            os.remove(media)
+        else:
+            await client.send_message(dessend_to , caption)
+            
+            
+  
+############ TANGO ###############
+
+@client.on(events.NewMessage(chats=tanchat))
+async def hello(event):
+    # chat = await event.get_chat()
+    caption = event.message.message
+    urls_to_change = re.findall('https?://mdisk.me/convertor/.*' , caption)
+    if(urls_to_change):
+        try:
+            media = await client.download_media(event.message)
+        except:
+            print("no media")
+        #  this is for blacklist word 
+        caption = re.sub("hehe" , "" , caption)
+        caption = re.sub("hoho" , "" , caption)
+
+        caption = re.sub("@.*" , "" , caption)
+        caption = re.sub("https://t.me/.*" , "" , caption)
+        caption = re.sub("t.me/.*" , "" , caption)
+
+        regrex_pattern = re.compile(pattern = "["
+                    u"\U0001F600-\U0001F64F"  # emoticons
+                    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                    u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                            "]+", flags = re.UNICODE)
+
+        # url to change 
+        for i in urls_to_change:
+            link = regrex_pattern.sub(r'' , i)
+            print(link)
+            url  = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
+            param = {
+                'token': mdisk_api,
+                'link':link
+                }
+            res = requests.post(url, json = param)
+            shareLink = res.json()['sharelink']
+            # print("changed link : " , shareLink)
+            caption = re.sub(link , shareLink , caption)
+            # print(caption)
+            sleep(0.2)
+        caption = caption + "\n" + footer
+        if media:
+            await client.send_file(tansend_to ,file=media , caption=caption)
+            os.remove(media)
+        else:
+            await client.send_message(tansend_to , caption)
+            
+        
+########### ONLYFANS #####################
+@client.on(events.NewMessage(chats=onchat))
+async def hello(event):
+    # chat = await event.get_chat()
+    caption = event.message.message
+    urls_to_change = re.findall('https?://mdisk.me/convertor/.*' , caption)
+    if(urls_to_change):
+        try:
+            media = await client.download_media(event.message)
+        except:
+            print("no media")
+        #  this is for blacklist word 
+        caption = re.sub("hehe" , "" , caption)
+        caption = re.sub("hoho" , "" , caption)
+
+        caption = re.sub("@.*" , "" , caption)
+        caption = re.sub("https://t.me/.*" , "" , caption)
+        caption = re.sub("t.me/.*" , "" , caption)
+
+        regrex_pattern = re.compile(pattern = "["
+                    u"\U0001F600-\U0001F64F"  # emoticons
+                    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                    u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                            "]+", flags = re.UNICODE)
+
+        # url to change 
+        for i in urls_to_change:
+            link = regrex_pattern.sub(r'' , i)
+            print(link)
+            url  = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
+            param = {
+                'token': mdisk_api,
+                'link':link
+                }
+            res = requests.post(url, json = param)
+            shareLink = res.json()['sharelink']
+            # print("changed link : " , shareLink)
+            caption = re.sub(link , shareLink , caption)
+            # print(caption)
+            sleep(0.2)
+        caption = caption + "\n" + footer
+        if media:
+            await client.send_file(onsend_to ,file=media , caption=caption)
+            os.remove(media)
+        else:
+            await client.send_message(onsend_to , caption)
+
+           
+
+
     
 print("Bot has been deployed.!")
 
 client.start()
 client.run_until_disconnected()
-
 
 
 
